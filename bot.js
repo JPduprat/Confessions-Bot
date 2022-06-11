9499afb7ffd05a0dbdb3318bfcaadceb36685903
JPduprat <jp_duprat@hotmail.com>
2022-06-11 19:29:14 -0400

    Embed adicionado

--- bot.js
+++ bot.js
@@ -2,21 +2,27 @@ require('dotenv').config()
 
 const Discord = require('discord.js')
 
-
 const { Client, Intents } = require('discord.js');
 
 const client = new Client({ partials: ["CHANNEL"], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.DIRECT_MESSAGES,Intents.FLAGS.DIRECT_MESSAGE_TYPING] });
 
 
-
+ //INICIO DO BOT
 client.on('ready', () => {
     console.log("Estou online! Enviem suas confissões em meu pv.");
     client.channels.cache.get("984278432315760660").send("***Estou online! Enviem suas confissões em meu pv***");
 })
 
+const { MessageEmbed } = require('discord.js');
+
+const exampleEmbed = new MessageEmbed()
+    .setColor('RANDOM')
+    .setTitle(':man_detective:・Confissão')
+    .setDescription("");
+
+
+//CONFISSÃO + COOLDOWN
 const cooldown = new Set();
-
-
 
 client.on('message', msg => {
     if (msg.guild === null && msg.author.id!=='983486932099137586') {
@@ -25,7 +31,7 @@ client.on('message', msg => {
         } else {
         var saymsg = msg.content;
         msg.channel.send(saymsg);
-        client.channels.cache.get("984278432315760660").send("══━✥◈✥━✥◈✥━══࿇══━✥◈✥━✥◈✥━══\nConfissão anônima:\n\n" + saymsg );
+        client.channels.cache.get("984278432315760660").send({ embeds: [exampleEmbed.setDescription(saymsg)] })
         
         cooldown.add(msg.author.id);
         setTimeout(() => {
